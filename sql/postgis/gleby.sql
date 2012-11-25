61cfd1281619b535bbfb8de9c7f89a7209a4cfb8
milosz.piglas <milosz.piglas@localhost>
2012-11-25 20:36:31 +0100

    poprawki w schemacie bazy postgis

--- sql/postgis/gleby.sql
+++ sql/postgis/gleby.sql
@@ -1,14 +1,19 @@
-﻿-- drop table gleby
-CREATE TABLE gleby -- gleby 
+﻿--drop table gleby
+CREATE TABLE gleba_dane -- gleby 
 (
   id integer NOT NULL,
   stanowisko integer NOT NULL,
-  piaszczysta character varying(1) not null default 'N',
-  gliniasta character varying(1) not null default 'N',
-  bagnista character varying(1) not null default 'N',
-  kamienistosc character varying(1) null, -- Mala, Srednia, Duza
-  uwagi character varying(255) null,
-  CONSTRAINT pk_gleby PRIMARY KEY (id),
-  constraint fk_stanowisko_gleby foreign key (stanowisko) references stanowiska (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE cascade
+  luzna character varying(1) check (luzna in ('T','N')),
+  zwiezla character varying(1) check (zwiezla in ('T','N')),
+  torf_bag character varying(1) check (torf_bag in ('T','N')),
+  kamienistosc character varying(1) check (kamienistosc in ('M','S','D')),
+  uwagi character varying(255),
+    CONSTRAINT gleba_dane_pkey PRIMARY KEY (id),
+  constraint gleba_dane_st_fkey foreign key (stanowisko) references stanowiska (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE cascade,
+      constraint unique_gleba_dane_st unique(id, stanowisko)
+)
+WITH (
+  OIDS=FALSE
 );
+ALTER TABLE gleba_dane OWNER TO milosz;
--- sql/postgis/zagrozenia.sql
+++ sql/postgis/zagrozenia.sql
@@ -17,6 +17,7 @@ CREATE TABLE ZAGROZENIA(
     id integer not null,
     stanowisko integer not null,
     wystepowanie varchar(1) not null, -- istnieje / nie istnieje
+    czas varchar(1), -- stale / dorazne
     przyczyna varchar(1), -- ludzie / natura
     uzytkownik varchar(1), -- wypelniane jezeli przyczyna == ludzie: uzytkownik spoleczny / prywatny
     uwagi varchar(255),
@@ -26,5 +27,6 @@ CREATE TABLE ZAGROZENIA(
     CONSTRAINT check_wystepowanie CHECK (wystepowanie in ('I','N')),
     CONSTRAINT check_przyczyna CHECK (przyczyna in ('L','N')),
     CONSTRAINT check_uzytkownik CHECK (uzytkownik in ('S','P')),
+    CONSTRAINT check_czas CHECK (czas in ('S','D')),
     constraint unique_zagrozenia_st unique(id, stanowisko)
 );
